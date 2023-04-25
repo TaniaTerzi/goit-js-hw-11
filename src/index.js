c2b2e23faee512d333ab96fd93fb2a127c89bdee
TaniaTerzi <terzitetyana@gmail.com>
2023-04-25 12:16:08 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 
 
-const input = document.querySelector('#search-form');
-console.log(input);
+const getInput = document.querySelector('#search-form');
+console.log(getInput);
 
 const button = document.querySelector('button');
 console.log(button);
+
+
+const getInputValue = (event => {
+    const inputValue = event.target.value;
+    console.log(inputValue);
+});
+
+getInput.addEventListener('input', getInputValue);
