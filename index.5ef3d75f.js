9567cf49d307b1f4b07a81e28b16b0668cb282d2
TaniaTerzi <118296956+TaniaTerzi@users.noreply.github.com>
2023-04-25 20:08:18 +0000

    Deploying to gh-pages from @ TaniaTerzi/goit-js-hw-11@e539dea6fff3c2aee6f9707ee82e8ae7216e6583 🚀

--- index.5ef3d75f.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector("#search-form");console.log(o);const e=document.querySelector("button");console.log(e);o.addEventListener("input",(o=>{const e=o.target.value;console.log(e)}));
-//# sourceMappingURL=index.5ef3d75f.js.map
--- index.a4d291f7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#search-form");console.log(e);var o=document.querySelector("button");console.log(o);e.addEventListener("input",(function(e){var o=e.target.value;console.log(o)}))}();
-//# sourceMappingURL=index.a4d291f7.js.map
